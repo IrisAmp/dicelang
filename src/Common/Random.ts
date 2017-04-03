9e2b4615a2083d57bc3afe31ce46910871240a78
Brandon Yue <bryue@microsoft.com>
2017-04-03 17:21:29 -0500

    Add RNG

--- src/Common/Random.ts
+++ src/Common/Random.ts
@@ -1,6 +1,23 @@
-const cryptObj = window.crypto || (window as any).msCrypto;
-if (!cryptObj) {
-  console.error('Environment does not support the Web Cryptography API.');
+let getRandomValues;
+try {
+  // tslint:disable-next-line:no-var-requires
+  getRandomValues = require('get-random-values');
+} catch (e) {
+  console.error(new Error('Enviroment does not support the crypto API.'));
 }
 
-export default cryptObj;
+const buff = new ArrayBuffer(4);
+export function getRandomNumber(): number {
+  if (getRandomValues) {
+    getRandomValues(new Uint8Array(buff));
+    return new Uint32Array(buff)[0] / 4294967295;
+  } else {
+    return Math.random();
+  }
+}
+
+export function getRandomInt(min: number, max: number): number {
+  min = Math.round(min);
+  max = Math.round(max);
+  return Math.floor(getRandomNumber() * (max - min)) + min + 1;
+}
