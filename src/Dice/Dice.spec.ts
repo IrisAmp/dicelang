cbb41c8a3e95309e9ab162bb3a8094b0a06d3056
Brandon Yue <bryue@microsoft.com>
2017-04-07 17:14:06 -0500

    Modified bounds checking in Dice and add tests.

--- src/Dice/Dice.spec.ts
+++ src/Dice/Dice.spec.ts
@@ -1,9 +1,37 @@
 import { Dice } from './Dice';
 
 describe('Dice', () => {
-  it('should be able to create dice from expressions', () => {
-    const expression = "";
-    const expected: Dice[] = [];
-    expect(Dice.fromExpression(expression)).toEqual(expected);
+  describe('static roll', () => {
+    it('should only roll 1 dice when given no value of n', () => {
+      expect(Dice.roll(3).length).toBe(1);
+      expect(Dice.roll(4).length).toBe(1);
+      expect(Dice.roll(6).length).toBe(1);
+      expect(Dice.roll(8).length).toBe(1);
+      expect(Dice.roll(10).length).toBe(1);
+      expect(Dice.roll(12).length).toBe(1);
+      expect(Dice.roll(20).length).toBe(1);
+    });
+
+    it('should roll the correct number of dice.', () => {
+      expect(Dice.roll(6, 1).length).toBe(1);
+      expect(Dice.roll(6, 2).length).toBe(2);
+      expect(Dice.roll(6, 3).length).toBe(3);
+      expect(Dice.roll(6, 100).length).toBe(100);
+      expect(Dice.roll(6, 1000).length).toBe(1000);
+    });
+
+    it('should never roll anything outside its bounds', () => {
+      /* Tests with randomness? NotLikeThis */
+      Dice.roll(6, 100).forEach((roll) => {
+        expect(roll).toBeGreaterThanOrEqual(1, `Rolled ${roll} on a d6`);
+        expect(roll).toBeLessThanOrEqual(6, `Rolled ${roll} on a d6`);
+      });
+    });
+
+    it('should throw when given an invalid value for d', () => {
+      expect(() => { Dice.roll(0); }).toThrowError(/0/);
+      expect(() => { Dice.roll(-1); }).toThrowError(/\-1/);
+      expect(() => { Dice.roll(Infinity); }).toThrowError(/Infinity/);
+    });
   });
 });
--- src/Dice/Dice.ts
+++ src/Dice/Dice.ts
@@ -1,9 +1,8 @@
 import { getRandomInt } from '../Common/Random';
 
 export class DiceMod {
-  public static fromModExpression(expr: string): DiceMod {
-    const result = new DiceMod();
-    return result;
+  protected static get modRegExpr(): RegExp {
+    return /./i;
   }
 
   public constructor(modExpr?: string) {
@@ -23,11 +22,13 @@ export class DiceMod {
 
   /**
    * A dice has been rolled and there are no more rolls to make. Modify the
-   * final result in place if necessary.
+   * final result in place if necessary, and return the combined result of the
+   * rolls.
    * @param {number[]} result The result array to modify.
+   * @returns {number} The combined total of the roll.
    */
-  public modResult(result: number[]) {
-    //
+  public modResult(result: number[]): number {
+    return result.reduce((acc, val) => acc + val, 0);
   }
 
   public toString(): string {
@@ -35,17 +36,39 @@ export class DiceMod {
   }
 }
 
+/**
+ * Representation of a dice roll.
+ */
 export class Dice {
+  /**
+   * The maximum allowed value of d, which is equal to the maximum value of a
+   * UInt32 (4294967295).
+   */
+  public static get maxD(): number {
+    return 4294967295;
+  }
+
+  /**
+   * The maximum allowed value of n, which is equal to the largest representable
+   * integer in JavaScript (9007199254740991, or Number.MAX_SAFE_INTEGER).
+   */
+  public static get maxN(): number {
+    return 9007199254740991;
+  }
+
+  /**
+   * Perform a simple dice roll.
+   * @param {number} d The number of faces the dice should have.
+   * @param {number} n The number of dice to roll. Defaults to 1 if not given.
+   * @throws If the value of n is not between 1 and Dice.maxN inclusive.
+   * @throws If the value of d is not between 1 and Dice.maxD inclusive.
+   */
   public static roll(d: number, n?: number): number[] {
-    if (d < 1) {
-      throw new Error(`d must be a natural number (got ${d})`);
-    }
+    Dice.checkD(d);
     if (n === undefined) {
       n = 1;
     }
-    if (n < 1) {
-      throw new Error(`n must be a natural number (got ${n})`);
-    }
+    Dice.checkN(n);
 
     const result: number[] = [];
     for (let i = 0; i < n; ++i) {
@@ -63,51 +86,170 @@ export class Dice {
     return /^(\d+)?d(f|\d+)(.*)$/i;
   }
 
+  private static checkD(d: number) {
+    if (d < 1 || d > Dice.maxD) {
+      throw new Error(`The value of n must be between 1 and ${Dice.maxD} (got ${d})`);
+    }
+  }
+
+  private static checkN(n: number) {
+    if (n < 1 || n > Dice.maxN) {
+      throw new Error(`The value of n must be between 1 and ${Dice.maxN} (got ${n})`);
+    }
+  }
+
   private _minRoll: number = 1;
   private _n: number;
   private _d: number;
-  private _fate: boolean;
+  private _fate: boolean = false;
   private _mod: DiceMod;
   private _binding: any;
-  private _lastRolls: number[];
+  private _rolls: number[];
 
   /**
    * Create a new dice from the given atomic expression.
-   * @param {string} diceExpr The atomic dice expression to create the dice from.
-   * @param {object} binding An object to bind to if the expression includes bindings.
+   * @param {?string} diceExpr The atomic dice expression to create the dice
+   * from. If not given, the dice is initialized as "1d20".
+   * @param {?object} binding An object to bind to if the expression includes
+   * bindings.
+   * @throws If the value of n is not between 1 and Dice.maxN inclusive.
+   * @throws If the value of d is not between 1 and Dice.maxD inclusive.
    */
-  public constructor(diceExpr: string, binding?: object) {
-    const parseResult = Dice.diceRegExp.exec(diceExpr);
-    if (parseResult == null) {
-      throw new Error(`"${diceExpr}" is not a valid dice expression.`);
-    }
-    this._mod = new DiceMod(parseResult[3]);
-    if (parseResult[1] === undefined) {
+  public constructor(diceExpr?: string, binding?: object) {
+    if (!diceExpr) {
+      this._d = 20;
       this._n = 1;
+      this._mod = new DiceMod();
     } else {
-      this._n = parseInt(parseResult[1], 10);
-    }
-    if (parseResult[2] === 'f') {
-      this._minRoll = -1;
-      this._d = 1;
-      this._fate = true;
-    } else {
-      this._d = parseInt(parseResult[2], 10);
-      this._fate = false;
+      const parseResult = Dice.diceRegExp.exec(diceExpr);
+      if (parseResult == null) {
+        throw new Error(`"${diceExpr}" is not a valid expression.`);
+      }
+
+      // Parse the mods to fail-fast if there is a problem with the modstring.
+      this._mod = new DiceMod(parseResult[3]);
+
+      // Parse the value of n
+      if (parseResult[1] === undefined) {
+        this._n = 1;
+      } else {
+        const n = parseInt(parseResult[1], 10);
+        Dice.checkN(n);
+        this._n = n;
+      }
+
+      // Parse the value of d.
+      if (parseResult[2] === 'f') {
+        // Use the fate setter.
+        this.fate = true;
+      } else {
+        const d = parseInt(parseResult[2], 10);
+        Dice.checkD(d);
+        this._fate = false;
+        this._d = d;
+      }
     }
   }
 
-  public roll(): number[] {
-    const result = [];
-    for (let i = 0; i < this._n; ++i) {
-      const roll = getRandomInt(this._minRoll, this._d);
-      this._mod.rolled(roll, result, this);
+  /**
+   * Get the value of n, which is the number of times the d-sided dice is rolled
+   * when roll() is called.
+   */
+  public get n(): number {
+    return this._n;
+  }
+
+  /**
+   * Set the value of n.
+   * @throws If the value of n is not between 1 and Dice.maxN inclusive.
+   */
+  public set n(value: number) {
+    value = Math.round(value);
+    Dice.checkN(value);
+    this._n = value;
+  }
+
+  /**
+   * The value of d is the number of faces on this dice. In the case of fate
+   * dice, returns 3 (d3).
+   */
+  public get d(): number {
+    if (this._fate) {
+      return 3;
+    } else {
+      return this._d;
     }
-    this._mod.modResult(result);
-    return result;
+  }
+
+  /**
+   * Set the value of d. If the dice is a fate dice, the fate attribute is
+   * cleared.
+   * @throws If the value of d is not between 1 and Dice.maxD inclusive.
+   */
+  public set d(value: number) {
+    value = Math.round(value);
+    Dice.checkD(value);
+    this._d = value;
+    this._fate = false;
+    this._minRoll = 1;
+  }
+
+  /**
+   * True if this dice is a fate dice.
+   */
+  public get fate(): boolean {
+    return this._fate;
+  }
+
+  /**
+   * Set if this dice is a fate dice. If the value changes to true, the previous
+   * value for d is cleared. If the value changes to false, the value of d is
+   * set to 3 (the dice becomes a d3 and keeps its value of n). If the new value
+   * is the same as the dice's current value, there is no effect.
+   */
+  public set fate(value: boolean) {
+    value = !!value;
+    if (value !== this._fate) {
+      this._fate = value;
+      if (value) {
+        this._d = 1;
+        this._minRoll = -1;
+      } else {
+        this._d = 3;
+        this._minRoll = 1;
+      }
+    }
+  }
+
+  /**
+   * Roll the dice. Returns the combined result of the dice roll. Individual
+   * rolls are stored in the rolls property.
+   * @param {?number} n The number of dice to roll. If not supplied, defaults to
+   * the value of n given when the dice was constructed.
+   * @returns {number} The combined result of the dice roll.
+   * @throws If the value of n is not between 1 and Dice.maxN inclusive.
+   */
+  public roll(n: number = this.n): number {
+    Dice.checkN(n);
+    const rolls = [];
+    this._rolls.length = 0;
+    for (let i = 0; i < n; ++i) {
+      const roll = getRandomInt(this._minRoll, this._d);
+      this._rolls.push(roll);
+      this._mod.rolled(roll, rolls, this);
+    }
+    return this._mod.modResult(rolls);
+  }
+
+  /**
+   * The raw values of the last dice roll, before they were modified using this
+   * Dice's DiceMod.
+   */
+  public get rolls(): number[] {
+    return this._rolls.slice();
   }
 
   public toString(): string {
-    return `${this._n}d${this._fate ? 'f' : this._d}${this._mod.toString()}`;
+    return `${this._n}d${this._fate ? 'F' : this._d}${this._mod.toString()}`;
   }
 }
